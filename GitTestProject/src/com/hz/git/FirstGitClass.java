ee44b05a14b825f200663f601f8450979fd3234f
Coding_byHz <1215495343@qq.com>
2019-08-07 15:02:08 +0800

    修改GitClass文件内容

--- GitTestProject/src/com/hz/git/FirstGitClass.java
+++ GitTestProject/src/com/hz/git/FirstGitClass.java
@@ -7,6 +7,7 @@ public class FirstGitClass {
         System.out.println("hello gitProject");
         System.out.println("hello gitProject");
         System.out.println("hello gitProject");
+        //再次做出修改
     }
 
     public void test(){
