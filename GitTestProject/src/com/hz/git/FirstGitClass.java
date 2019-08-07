8eab01cce0f480710c59e1bb465295b7964eec95
Coding_byHz <1215495343@qq.com>
2019-08-07 14:49:27 +0800

    将工程添加到本地出库

---
+++ GitTestProject/src/com/hz/git/FirstGitClass.java
@@ -0,0 +1,11 @@
+package com.hz.git;
+
+public class FirstGitClass {
+    public static void main(String[] args) {
+        System.out.println("hello gitProject");
+        System.out.println("hello gitProject");
+        System.out.println("hello gitProject");
+        System.out.println("hello gitProject");
+        System.out.println("hello gitProject");
+    }
+}
